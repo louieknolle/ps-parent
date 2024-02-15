8fce468be2e8aabfd0705e4c87da08f09e668edf
Louie Knolle <knollelw@gmail.com>
2024-02-14 20:27:23 -0500

    more top padding css issues

--- app/src/components/Layout.tsx
+++ app/src/components/Layout.tsx
@@ -4,8 +4,8 @@ import HomeButton from './HomeButton'
 
 const Layout = () => {
   return (
-    <div className="app h-screen bg-gradient-to-r from-sky-500 to-indigo-500 pt-8 flex justify-center">
-      <nav>
+    <div className="app h-full bg-gradient-to-r from-sky-500 to-indigo-500 pt-8 lg:pt-8 flex justify-center">
+      <nav className="">
         <HomeButton />
       </nav>
       <main>
